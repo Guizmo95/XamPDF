ad9d6b85a1ecb1f02e496f10d3ebfcab09e4f5d2
rjebbari <jebbarirayhane@gmail.com>
2020-05-12 00:05:06 +0200

    icon

--- Pdf/Pdf.Android/obj/Release/90/android/environment.armeabi-v7a.s
+++ Pdf/Pdf.Android/obj/Release/90/android/environment.armeabi-v7a.s
@@ -89,7 +89,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",%progbits,1
 	.type	.L.str.8, %object
 .L.str.8:
-	.asciz	"40e1ad40-f675-4208-8249-4e5cb3acc650"
+	.asciz	"a4321446-86eb-4d85-9b09-fa12b1f48e98"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",%progbits,1
 	.type	.L.str.9, %object
